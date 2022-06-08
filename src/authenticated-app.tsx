a32e88a97e69176575c022ed38f9e0bee7877d4e
masterJediYyf <1786830944@qq.com>
2022-06-08 23:44:19 +0800

    style: 优化列表页

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,3 +1,4 @@
+import styled from "@emotion/styled";
 import { useAuth } from "context/auth-context";
 import React from "react";
 import { ProjectListScreen } from "views/projiect-list";
@@ -6,9 +7,45 @@ export const AuthenticatedApp = () => {
   const { logout } = useAuth();
 
   return (
-    <div>
-      <button onClick={logout}>登出</button>
-      <ProjectListScreen />
-    </div>
+    <Container>
+      <Header>
+        <HeaderLeft>
+          <h3>Logo</h3>
+          <h3>项目</h3>
+          <h3>用户</h3>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={logout}>登出</button>
+        </HeaderRight>
+      </Header>
+      <Main>
+        <ProjectListScreen />
+      </Main>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr 6rem;
+  height: 100vh;
+  width: 100vw;
+`;
+
+const Header = styled.header`
+  grid-area: header;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const HeaderRight = styled.div``;
+
+const Main = styled.main`
+  grid-area: main;
+`;
