cb8cfc3aa54129ebad7db70c8c74d99e0bff5495
masterJediYyf <1786830944@qq.com>
2022-06-09 22:34:43 +0800

    style: 用Row布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { Row } from "components/lib";
 import { useAuth } from "context/auth-context";
 import React from "react";
 import { ProjectListScreen } from "views/projiect-list";
@@ -8,11 +9,11 @@ export const AuthenticatedApp = () => {
 
   return (
     <Container>
-      <Header>
-        <HeaderLeft>
-          <h3>Logo</h3>
-          <h3>项目</h3>
-          <h3>用户</h3>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h2>Logo</h2>
+          <h2>项目</h2>
+          <h2>用户</h2>
         </HeaderLeft>
         <HeaderRight>
           <button onClick={logout}>登出</button>
@@ -29,23 +30,12 @@ const Container = styled.div`
   display: grid;
   grid-template-rows: 6rem 1fr 6rem;
   height: 100vh;
-  width: 100vw;
 `;
 
-const Header = styled.header`
-  grid-area: header;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-`;
+const Header = styled(Row)``;
 
-const HeaderLeft = styled.div`
-  display: flex;
-  align-items: center;
-`;
+const HeaderLeft = styled(Row)``;
+
 const HeaderRight = styled.div``;
 
-const Main = styled.main`
-  grid-area: main;
-`;
+const Main = styled.main``;
